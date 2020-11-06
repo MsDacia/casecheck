2141efd9a5587ee935e500c92fdc33420088178c
Dacia Rodrigue <msdessin@gmail.com>
2020-11-05 19:02:50 -0600

    added tests

---
+++ tests/e2e/specs/searchQuoteGardens.js
@@ -0,0 +1,28 @@
+describe('Access Quote Gardens landing page', () => {
+	it('should view the root url', () => {
+		cy.visit('/')
+	})
+
+	it('should have title', () => {
+		cy.contains('[data-test=title]', 'Quote Gardens')
+	})
+
+	it('should be able to execute a search via submit button', () => {
+		cy.get('[data-test=search-quote-gardens]')
+			.type('test')
+			.trigger('blur')
+			.should('have.value', 'test')
+		cy.get('[data-test=search-quote-grands-button]').click()
+	})
+
+	it('should be able to execute a search via enter key press', () => {
+		cy.get('[data-test=search-quote-gardens]')
+			.type('test{enter}')
+
+		it('should have search results', () => {
+			cy.get('[data-test=quote-gardens-results')
+				.should('be.visible')
+		})
+	})
+
+})
---
+++ tests/unit/specs/search.spec.js
@@ -0,0 +1,4 @@
+import { shallowMount } from '@vue/test-utils'
+import SearchQuoteGardens from './SearchQuoteGardens.vue'
+
+const wrapper = shallowMount(SearchQuoteGardens)
